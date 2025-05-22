dc4777bc4a1f813e7f4d1151286c102d29ad36c3
martinapanini <martina.panini@studenti@unitn.it>
2025-05-22 08:38:12 +0200

    Revert "Salvataggio delle modifiche locali prima del revert"
    This reverts commit b9ddf37ad3af71b430baf51984de26bdad692fbe.

--- BDI/main.js
+++ BDI/main.js
@@ -3,9 +3,8 @@ import { client} from './client.js';
 import {AStarMove, GoPickUp, GoDeliver, SmartExplore, ExploreSpawnTiles} from './plan.js'
 import { PddlMove} from './pddl_plan.js';
 import {IntentionRevisionReplace} from './intentions.js'
-import { pickupCoordination } from './shared.js';
 import argsParser from 'args-parser';
-import { me, ally } from './sensing.js'; // Import me and ally
+import { me, ally } from './sensing.js'; 
 import { multiAgent } from './index.js';
 import { sharePerception } from './communication.js';
 
@@ -18,20 +17,20 @@ export const teamAgentId = args.teamId;
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-// Ricezione messaggi per la coordinazione del pickup
-client.onMsg(async (id, name, msg, reply) => {
-    if (msg?.action === 'pickup') {
-        await sleep(Math.random() * 50);
-        if (reply) {
-            if (pickupCoordination[msg.parcelId] === client.id) {
-                reply(false); // io sto già andando
-            } else {
-                pickupCoordination[msg.parcelId] = id;
-                reply(true); // vai pure tu
-            }
-        }
-    }
-});
+// // Ricezione messaggi per la coordinazione del pickup
+// client.onMsg(async (id, name, msg, reply) => {
+//     if (msg?.action === 'pickup') {
+//         await sleep(Math.random() * 50);
+//         if (reply) {
+//             if (pickupCoordination[msg.parcelId] === client.id) {
+//                 reply(false); 
+//             } else {
+//                 pickupCoordination[msg.parcelId] = id;
+//                 reply(true);
+//             }
+//         }
+//     }
+// });
 
 setInterval(() => {
     if (teamAgentId) sharePerception(teamAgentId);
